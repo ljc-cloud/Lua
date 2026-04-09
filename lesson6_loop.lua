da2fe8055554ccdf121d2e594213f64edc4935a8
xiancai <2066765685@qq.com>
2026-04-09 20:01:04 +0800

    循环语句学习提交

---
+++ lesson6_loop.lua
@@ -0,0 +1,23 @@
+print("******循环语句******")
+
+print("******while循环******")
+num = 1
+-- while 进入循环条件 do 语句块 end
+while num <= 5 do
+    print(num)
+    num = num + 1
+end
+
+print("******do while循环******")
+num = 0
+-- repeat 语句块 until 结束循环条件 
+repeat
+    print(num)
+    num = num + 1
+until num > 5
+
+print("******for循环******")
+
+for i = 1, 5 do
+    print(i)
+end
